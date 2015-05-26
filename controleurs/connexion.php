c5bd26fde296829a16468b29fbd34f8d97c987ae
KarenDes <karen09@hotmail.fr>
2015-05-26 17:05:24 +0200

    SUPER COMMITTTT

---
+++ controleurs/connexion.php
@@ -0,0 +1,7 @@
+<?php
+
+/*require('modeles/connexion.php'); */
+
+require('vues/connexion.php');
+
+?>
---
+++ controleurs/inscription.php
@@ -0,0 +1,7 @@
+<?php
+
+/*require('modeles/inscription.php'); */
+
+require('vues/inscription.php');
+
+?>
