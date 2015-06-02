30e7ff6b6074770eb79a7eb59c5c75152f96bd53
JeremyPineau <jeremailpineau@gmail.com>
2015-06-02 14:33:36 +0200

    c est rien

--- static/js/depotverif.js
+++ static/js/depotverif.js
@@ -2,7 +2,7 @@
   $(document).ready(function(){
     
     var 
-        $date=$('#date');
+        $date=$('#dateexpiration');
         $prix=$('#prix');
         $envoi = $('#formulaire-depot'),
 
@@ -45,3 +45,137 @@ $("#PoidsQuant").change(function() {
     if ( $("#PoidsQuant").val() == "unite" ){ $("#resultat").text("l'unité"); }
 });
 
+<!--////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+<?php
+if(isset($_POST['validerDepot'])) {
+
+
+if (
+      (
+      (($_POST['nom_produit']==AutreAliment) AND  empty($_POST['ProduitAutre']))
+      OR
+      (($_POST['nom_produit']!=AutreAliment) AND  !empty($_POST['ProduitAutre'])) 
+      )
+    )
+      {
+?>
+        
+        $(nom_produit).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red' 
+        });
+        $(ProduitAutre).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+        
+        $(AfficheRange).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+        
+<?php ;     }
+
+
+if ($_POST['nbPoidsQuant']=="0")
+      {
+?>
+        
+        $(AfficheRange).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+       
+
+<?php ;     }
+
+
+if (($_POST['PoidsQuant']=="") OR ($_POST['PoidsQuant']==AucuneUnite))
+      {
+?>
+        
+        $(PoidsQuant).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+        
+        $(AfficheRange).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+        
+<?php ;     }
+
+
+
+if (($_POST['dateexpiration']>"2999/12/31") OR ($_POST['dateexpiration']<"2015/01/01"))
+      {
+?>
+        
+        $(dateexpiration).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+        
+        $(AfficheRange).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+        
+<?php ;     }
+
+
+
+if (empty($_POST['prix']) AND empty($_POST['troc']))
+      {
+?>
+        
+        $(prix).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+        $(troc).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+        
+        
+        $(AfficheRange).css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+        
+<?php ;     }
+
+
+if (
+      (
+      (($_POST['nom_produit']==AutreAliment) AND  !empty($_POST['ProduitAutre']))
+      OR
+      (($_POST['nom_produit']!=AutreAliment) AND  empty($_POST['ProduitAutre'])) 
+      )
+      AND ($_POST['nbPoidsQuant']!=0)
+      AND ($_POST['PoidsQuant']!=AucuneUnite)
+      AND (($_POST['dateexpiration']<"2999/12/31") AND ($_POST['dateexpiration']>"2015/01/01"))
+      AND ((empty($_POST['prix']) AND !empty($_POST['troc'])) OR (!empty($_POST['prix']) AND empty($_POST['troc'])) OR (!empty($_POST['prix']) AND !empty($_POST['troc'])))
+  )
+{
+
+  echo "<script type='text/javascript'>document.location.replace('http://localhost/dreamfield_mvc/?page=ProfilUtilisateur.php');</script>";
+  $req = $bdd->prepare('INSERT INTO `DREAMFIELD`.`annonces` (`nom_produit`, `ProduitAutre`, `nbPoidsQuant`, `PoidsQuant`, `dateexpiration`, `remarque`, `prix`, `troc`) VALUES (:nom_produit, :ProduitAutre, :nbPoidsQuant, :PoidsQuant, :dateexpiration, :remarque, :prix, :troc)');
+  $req->execute(array(
+    
+    'nom_produit' => $_POST['nom_produit'],
+    'ProduitAutre' => $_POST['ProduitAutre'],
+    'nbPoidsQuant' => $_POST['nbPoidsQuant'],
+    'PoidsQuant' => $_POST['PoidsQuant'],
+    'dateexpiration' => $_POST['dateexpiration'],
+    'remarque' => $_POST['remarque'],
+    'prix' => $_POST['prix'],
+    'troc' => $_POST['troc'],
+    ));
+}
+
+}
+?>
