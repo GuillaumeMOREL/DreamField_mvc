457c0e715204fd7bd7fc9c996489ef7ce9a439ed
GuillaumeMOREL <guimorel.gm@gmail.com>
2015-06-06 23:25:33 +0200

    Css editionprofil

--- vues/editionprofil.php
+++ vues/editionprofil.php
@@ -1,44 +1,45 @@
-<div class="newprofil" align ="center">
+<div id="newprofil_editionprofil" >
+  <div id="newprofil-bis_editionprofil">
     <form id="newform" method="post" action="" enctype="multipart/form-data">
    
     <p>
 
-              <label for="newadresse">Adresse :</label>
-              <input type="text" name="newadresse" id="newadresse" class="newchamp" size="30" placeholder="Ex :2 bis rue du long des bois" value="<?php echo $result['adresse']; ?>" />
+              <label for="newadresse" class="label_editionprofil">Adresse :</label>
+              <input class="input_editionprofil" type="text" name="newadresse" id="newadresse" placeholder="Ex :2 bis rue du long des bois" value="<?php echo $result['adresse']; ?>" />
               <br />
               <br />
-              <label for="newnumero_departement"> Num&eacute;ro d&eacute;partement :</label>
-              <input type="number" name="newnumero_departement" id="newnumero_departement" class="newchamp" placeholder="Ex : 78" value="<?php echo $result['numero_departement']; ?>" min="01" max="95"/>
+              <label for="newnumero_departement" class="label_editionprofil"> Num&eacute;ro d&eacute;partement :</label>
+              <input class="input_editionprofil" type="number" name="newnumero_departement" id="newnumero_departement" placeholder="Ex : 78" value="<?php echo $result['numero_departement']; ?>" min="01" max="95"/>
               <br />
               <br />
-              <label for="newmail"> Email :</label>
-              <input type="email" name="newmail" id="newmail" class="newchamp" size="30" placeholder="Ex :azerty@gmail.com " value="<?php echo $result['mail']; ?>" />
+              <label for="newmail" class="label_editionprofil"> Email :</label>
+              <input class="input_editionprofil" type="email" name="newmail" id="newmail" placeholder="Ex :azerty@gmail.com " value="<?php echo $result['mail']; ?>" />
               <br />
               <br />
-              <label class="newgrand" for="newtel"> Numero de telephone :</label>
-              <input type="tel" name="newtel" id="newtel" value="<?php echo '0'.$result['tel']; ?>" maxlength="10" minlength="10"/>
+              <label  for="newtel" class="label_editionprofil"> Numero de telephone :</label>
+              <input class="input_editionprofil" type="tel" name="newtel" id="newtel" value="<?php echo '0'.$result['tel']; ?>" maxlength="10" minlength="10"/>
               <br />
               <br />
-              <label class="newgrand" for="newpass">Mot de passe :</label>
-              <input type="password" name="newpass" id="newpass" class="newchamp" minlength="6" />
+              <label  for="newpass" class="label_editionprofil">Mot de passe :</label>
+              <input class="input_editionprofil" type="password" name="newpass" id="newpass"  minlength="6" />
               <br />
               <br />
-              <label class="newgrand" for="newpassverif">Confirmez votre mot de passe :</label>
-              <input type="password" name="newpassverif" id="newpassverif" class="newchamp" minlength="6"/>
+              <label  for="newpassverif" class="label_editionprofil">Confirmez votre mot de passe :</label>
+              <input class="input_editionprofil" type="password" name="newpassverif" id="newpassverif"  minlength="6"/>
               <br />
               <br />
-              <label> Avatar :</label>
-              <input type="file" name="avatar" />
+              <label class="label_editionprofil"> Avatar :</label>
+              <input class="input-bis_editionprofil" type="file" name="avatar" />
               <br />
               <br />
 
       <div class="modif">
           <label for="modification">Validez les modifications faites :</label>
-          <input id="modification" type="submit" name="modification" value="Mettre à jour mon profil" />
+          <input class="input_editionprofil" id="modification" type="submit" name="modification" value="Mettre à jour mon profil" />
       </div>
 
     </form>
 
         <?php if(isset($msg)) { echo $msg; } ?>
-
+  </div>
 </div>
