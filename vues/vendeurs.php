abd8c37f04c7661d6f3353116e0965f3d08ea923
GuillaumeMOREL <guimorel.gm@gmail.com>
2015-06-09 23:07:44 +0200

    finition offres

--- vues/vendeurs.php
+++ vues/vendeurs.php
@@ -1,45 +1,39 @@
-<div style="margin-top:60px"></div>
-<div class="liste-offres">
+<div id="liste-offres_vendeurs">
 
-
-<h1> Voici les vendeurs dans le département que vous avez sélectionné.</h1>
-
-
+<div id="titre_vendeurs"> Voici les vendeurs dans le département que vous avez sélectionné :</div>
 
 <?php
-
 	foreach($dn as $annonces){
-
 ?> 
 
-
-    
 	<a href="#redirige sur profil du vendeurs ac l'offre" title="<?php echo $annonces['id_annonce'];?>">
-	<div class="box_vendeurs"> 
-		<div class="date">
-			<div><?php echo $annonces['date_annonce']; ?></div>
-		</div>	
-		<div class="image">
+		<div id="box_vendeurs"> 
+			<div id="box-bis_vendeurs">
 
-			<div class="image-and-nb">
-				<img src="/static/img/photo_fruit_legume/<?php echo $annonces['nom_produit'];?>.jpg" alt="<?php echo $annonces['id_annonce'];?>"/>
+			<div id="image_vendeurs">
+				<div class="image-bis_vendeurs">
+					<img id="img_vendeurs" src="/static/img/photo_fruit_legume/<?php echo $annonces['nom_produit'];?>.jpg" alt="<?php echo $annonces['id_annonce'];?>"/>
+				</div>
+			</div>
+
+			<div class="detail_vendeurs">
+				<h2 class="nom-vendeurs"><?php echo $annonces['prenom'].' '.$annonces['nom'];?></h2>
+
+				<div class="produit"><?php echo $annonces['nom_produit'];?> </div>
+
+				<div class="date">
+					<div><?php echo $annonces['date_annonce']; ?></div>
+				</div>	
+
+				<div class="quantite"><?php echo $annonces['nbPoidsQuant']. ' '.$annonces['PoidsQuant'];?></div>
+
+				<div class="prix"><?php echo $annonces['prix'];?> €</div>
 			</div>
 		</div>
-		<div class="detail">
-			<h2 class="nom-vendeurs"><?php echo $annonces['prenom'].' '.$annonces['nom'];?></h2>
-			<div class="produit"><?php echo $annonces['nom_produit'];?> </div>
-			<!-- <div class="categorie"><?php echo $annonces['categorie'];?></div> -->
-			<div class="quantite"><?php echo $annonces['nbPoidsQuant']. ' '.$annonces['PoidsQuant'];?></div>
-	 <!--   <div class="departement">#departement</div> -->
-			<div class="prix"><?php echo $annonces['prix'];?> €</div>
-			<div class="clear"></div> 
 		</div>
-	</div>
 	</a>
-	<div class="clear"></div>
-
 </div>
-<?php
 
+<?php
 }
 ?>
