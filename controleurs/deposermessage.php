c320d498b6e24c707a43d6ce3b00ea58a42daf3d
GuillaumeMOREL <guimorel.gm@gmail.com>
2015-06-11 17:54:37 +0200

    forum dossier re

---
+++ controleurs/deposermessage.php
@@ -0,0 +1,16 @@
+<?php
+
+  $req = $bdd->prepare('INSERT INTO commentaires(titre, id_sujet, username, contenu, date_de_publication) VALUES(:titre, :id_sujet, :username, :contenu, NOW())'); 
+
+  $req->execute(array(
+        'titre' => $_POST['titre'],
+        'id_sujet' => $_GET['id'],
+        'username' => $_POST['username'],
+        'contenu' => $_POST['contenu']));
+
+$id=$_GET['id'];
+header('Location: index.php?page=sujet&id='.$id);
+
+require('vues/deposermessage.php');
+
+?>
