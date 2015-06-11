30c96f2e091a40f151ff438bef587f824e47ee42
JeremyPineau <jeremailpineau@gmail.com>
2015-06-11 18:04:26 +0200

    vas y guigui

--- vues/recettes.php
+++ vues/recettes.php
@@ -1,4 +1,4 @@
 <div class="recettes">
 	<h2>Entrée fraicheur au tartare </h2>
-	<img src="static/img/photosRecettes/EntreeFraicheurAuTartare.jpg" href="vues/recette1.php"/>
+	<img src="static/img/photosRecettes/EntreeFraicheurAuTartare.jpg" href="?page=recette1.php"/>
 </div>
