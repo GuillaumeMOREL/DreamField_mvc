cecfeb1c6c75ab2f30f72dd22a4d8448d6593303
KarenDes <karen09@hotmail.fr>
2015-06-12 11:39:53 +0200

    SQLLL

--- static/sql/dreamfield.sql
+++ static/sql/dreamfield.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Jeu 11 Juin 2015 à 19:29
+-- Généré le :  Ven 12 Juin 2015 à 11:39
 -- Version du serveur :  5.5.38
 -- Version de PHP :  5.6.2
 
@@ -36,7 +36,7 @@ CREATE TABLE `annonces` (
   `extension_upload` varchar(5) NOT NULL,
   `id_vendeur` int(11) NOT NULL,
   `date_annonce` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `annonces`
@@ -44,9 +44,7 @@ CREATE TABLE `annonces` (
 
 INSERT INTO `annonces` (`id_annonce`, `nom_produit`, `ProduitAutre`, `nbPoidsQuant`, `prix`, `dateexpiration`, `PoidsQuant`, `remarque`, `troc`, `photo_offre`, `departement_annonce`, `adresse_image`, `extension_upload`, `id_vendeur`, `date_annonce`) VALUES
 (18, 'avocat', '', 11, 20, '2015-06-13', 'unite', 'De très bonne qualité, n''hésitez pas !', 'Argent de préférence', '', 78, 0, '', 28, '2015-06-09'),
-(19, 'cassis', '', 7, 13, '2015-06-14', 'kg', '. . .', 'en échange de fruits svp', '', 75, 0, '', 26, '2015-06-09'),
-(20, 'prune', '', 21, 30, '2015-06-15', 'unite', 'Vends plusieurs lots de prune..\r\n\r\n', 'Argent ou melon', '', 78, 0, '', 25, '2015-06-09'),
-(21, 'poireau', '', 15, 45, '2015-06-19', 'unite', '', 'Non', '', 78, 0, '', 25, '2015-06-09');
+(19, 'cassis', '', 7, 13, '2015-06-14', 'kg', '. . .', 'en échange de fruits svp', '', 75, 0, '', 26, '2015-06-09');
 
 -- --------------------------------------------------------
 
@@ -531,7 +529,7 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `annonces`
 --
 ALTER TABLE `annonces`
-MODIFY `id_annonce` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+MODIFY `id_annonce` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT pour la table `carte_de_france`
 --
